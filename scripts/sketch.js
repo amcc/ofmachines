7578192fcf3cd944baebaa682941c7fe2661ee28
amcc <alistairmcclymont@gmail.com>
2023-09-04 22:22:26 +0100

    feat: blocky basics

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,80 +1,30 @@
 let mobilenet;
-let video;
+let capture;
 let label;
 let probability;
-let resultOne = false;
 let resultNumber;
 let countDown = 5;
-let easing = 0.05;
-let barLength = 0;
-let lemonBar = 0;
-let videoLoaded;
 
 //pixelation
 let img;
-let vScale = 10;
-let grid = 11;
-let slider;
+let squareSize = 0;
+let camDiff = 0;
+let camShortSide = 0;
+let camWidth = 0;
+let camHeight = 0;
 
-function modelReady() {
-  console.log("model is ready");
-  mobilenet.predict(1000, gotResult);
-}
-// function imageReady() {
-//   image(puffin, 0,0,width, height);
-// }
-
-function gotResult(error, results) {
-  if (error) {
-    // console.error(error)
-  } else {
-    if (!resultOne) {
-      // console.log(results);
-      resultOne = true;
-    }
-
-    resultNumber = search("lemon", results);
-    // console.log('search = ' + resultNumber);
-
-    label = results[0].className;
-    label += "\n";
-    var firstProb = Math.round(results[0].probability * 1000000) / 10000;
-    firstProb = firstProb.toFixed(3);
-    if (firstProb < 10) {
-      firstProb = "0" + firstProb;
-    }
-    label += firstProb + "%";
-    label += "\n";
-    label += results[resultNumber].className;
-    label += "\n";
-    var secondProb =
-      Math.round(results[resultNumber].probability * 1000000) / 10000;
-    secondProb = Math.abs(secondProb.toFixed(3));
-    if (secondProb < 10) {
-      secondProb = "0" + secondProb;
-    }
-    label += secondProb + "%";
-
-    // label += "\n";
-    // label += resultNumber + " - " + (1000-resultNumber)/10 + "%";
-
-    // bar length as number out of 1000
-    // barLength = (1000-resultNumber)/1000 * width;
-
-    // bar length as percentage of best guess
-    barLength =
-      (results[resultNumber].probability / results[0].probability) * width;
-    // bar length as percentage of best guess with a bit of position
-    // barLength = results[resultNumber].probability/results[0].probability * width;
-  }
-}
+//text
+let predictionText = "";
+let predictionPercentage = "";
+let lemonPercentage = "";
 
 function setup() {
   // put setup code here
-  createCanvas(windowWidth, windowHeight);
-  // console.log("the width is " + width + " and the height is " + height);
+  let shortWindowSide = windowWidth > windowHeight ? windowHeight : windowWidth;
+  const canvas = createCanvas(shortWindowSide, shortWindowSide);
+  canvas.parent("canvas-container");
   label = "loading AI model";
-  video = createCapture(
+  capture = createCapture(
     {
       audio: false,
       video: {
@@ -82,46 +32,24 @@ function setup() {
       },
     },
     function () {
-      // videoLoaded = true;
-      // console.log('capture ready.')
+      // do things when video ready
     }
   );
-  video.elt.setAttribute("playsinline", "");
-  video.hide();
-  // video.size(w, h);
-  // canvas = createCanvas(w, h);
-  // video = createCapture(constraints);
-  // video.hide();
+  capture.elt.setAttribute("playsinline", "");
+  capture.hide();
   background(0);
-  mobilenet = ml5.imageClassifier("MobileNet", video, modelReady);
+  mobilenet = ml5.imageClassifier("MobileNet", capture, modelReady);
+
+  // get text ready
+  predictionText = select("#prediction-text");
+  predictionPercentage = select("#prediction-percentage");
+  lemonPercentage = select("#lemon-percentage");
 }
 
 function draw() {
-  clear();
-
-  let grid = 11;
-  let img = video.loadPixels();
-  let landscape = img.width > img.height;
-  let sideDif = landscape ? img.width - img.height : img.height - img.width;
-
-  let step = landscape ? Math.ceil(img.height / 11) : Math.ceil(img.width / 11);
-  console.log(step, img.width, img.height);
-
-  let vScale = Math.floor(width / img.width) / 1.1;
-
-  for (var y = 0; y < img.height; y += step) {
-    for (var x = sideDif / 2; x < img.width - sideDif / 2; x += step) {
-      var index = (img.width - x + 1 + y * img.width) * 4;
-      var r = img.pixels[index + 0];
-      var g = img.pixels[index + 1];
-      var b = img.pixels[index + 2];
-      var bright = (r + g + b) / 3;
-      noStroke();
-      fill(r, g, b);
-      // rectMode(CENTER);
-      rect(x * vScale, y * vScale, vScale * step, vScale * step);
-    }
-  }
+  makeCamImage();
+  // image(img, 0, 0, squareSize, squareSize);
+  pixelate();
 
   // background(0);
   // only predict every now and then
@@ -131,10 +59,10 @@ function draw() {
   }
   countDown--;
   // make the text
-  fill(255);
+  fill(0);
   textFont("Roboto Condensed");
   textSize(height / 19);
-  text(label, 20, height / 2);
+  // text(label, 20, height / 2);
 }
 
 function search(nameKey, myArray) {
@@ -143,4 +71,115 @@ function search(nameKey, myArray) {
       return i;
     }
   }
+}
+
+function modelReady() {
+  mobilenet.predict(1000, gotResult);
+}
+
+function gotResult(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    resultNumber = search("lemon", results);
+
+    let predictionProbability = percentagise(results[0].probability);
+    // Math.round(results[0].probability * 1000000) / 10000;
+    // predictionProbability = predictionProbability.toFixed(3);
+
+    let lemonProbability = percentagise(results[resultNumber].probability);
+    // Math.round(results[resultNumber].probability * 1000000) / 10000;
+
+    predictionText.html(results[0].className);
+    predictionPercentage.html(predictionProbability + "%");
+    lemonPercentage.html(lemonProbability + "%");
+  }
+}
+
+function percentagise(number) {
+  const percentage = Math.round(number * 1000000) / 10000;
+  const fixed = percentage.toFixed(3);
+  if (fixed < 10) {
+    return "0" + fixed;
+  } else {
+    return fixed;
+  }
+}
+
+function makeCamImage() {
+  if (capture.width !== camWidth || capture.height !== camHeight) {
+    debounce(() => getSizes());
+    camWidth = capture.width;
+    camHeight = capture.height;
+  }
+  getSizes();
+  // The capture element is initially smaller than it should be
+
+  img = createImage(squareSize, squareSize);
+
+  img.copy(
+    capture,
+    camDiff,
+    0,
+    camShortSide,
+    camShortSide,
+    0,
+    0,
+    squareSize,
+    squareSize
+  );
+}
+
+function pixelate() {
+  if (img) {
+    let pixImage = img.get();
+    pixImage.loadPixels();
+    let step = Math.ceil(squareSize / 11);
+    let vScale = 1;
+    for (var y = 0; y < pixImage.height; y += step) {
+      for (var x = 0; x < pixImage.width; x += step) {
+        var index = (x + y * pixImage.width) * 4;
+        var r = pixImage.pixels[index + 0];
+        var g = pixImage.pixels[index + 1];
+        var b = pixImage.pixels[index + 2];
+        // var bright = (r + g + b) / 3;
+        noStroke();
+        fill(r, g, b);
+        // rectMode(CENTER);
+        let rectSize = vScale * step;
+        rect(
+          squareSize - x * vScale - rectSize,
+          y * vScale,
+          rectSize,
+          rectSize
+        );
+      }
+    }
+  }
+}
+
+function getSizes() {
+  squareSize = width > height ? height : width;
+  camDiff =
+    capture.width > capture.height
+      ? Math.floor((capture.width - capture.height) / 2)
+      : Math.floor((capture.height - capture.width) / 2);
+  camShortSide =
+    capture.width > capture.height ? capture.height : capture.width;
+}
+
+function windowResized() {
+  let shortWindowSide = windowWidth > windowHeight ? windowHeight : windowWidth;
+  resizeCanvas(shortWindowSide, shortWindowSide);
+  debounce(() => getSizes());
+}
+
+function debounce(func, timeout = 300) {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, timeout);
+  };
 }
